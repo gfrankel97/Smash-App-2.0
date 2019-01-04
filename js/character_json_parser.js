5f6c5be5e8f58abed12c48eb1d7dc73c7dc99ea8
unknown <gabriel.frankel@louisville.edu>
2019-01-03 23:59:20 -0500

    replaced keys

--- js/character_json_parser.js
+++ js/character_json_parser.js
@@ -1,7 +1,7 @@
 function load_JSON(callback) {
     var xobj = new XMLHttpRequest();
         xobj.overrideMimeType("application/json");
-        xobj.open('GET', 'https://smashappstorage.file.core.windows.net/smashappfiles/images/moves/hitboxDB.json' + '?sv=2018-03-28&ss=f&srt=co&sp=r&st=2019-01-03T04%3A22%3A55Z&se=2019-04-04T04%3A22%3A00Z&sig=%2F1i7SsE%2FAqLKuGILJUrMZMfRwPE5DhufwFxow70rQgQ%3D', true);
+        xobj.open('GET', 'https://smashappstorage.file.core.windows.net/smashappfiles/images/moves/hitboxDB.json' + '?st=2019-01-04T04%3A38%3A14Z&se=2020-01-05T04%3A38%3A00Z&sp=rl&sv=2018-03-28&sr=s&sig=HEgPEvrg%2F34yozJPQPfkKQANMpifiQnsKNddAFGfi2s%3D', true);
     xobj.onreadystatechange = function () {
           if (xobj.readyState == 4 && xobj.status == "200") {
               callback(xobj.responseText);
@@ -13,7 +13,7 @@ function load_JSON(callback) {
 function load_JSON2(callback) {
     var xobj = new XMLHttpRequest();
         xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'https://smashappstorage.file.core.windows.net/smashappfiles/images/moves/character_general_data.json' + '?sv=2018-03-28&ss=f&srt=co&sp=r&st=2019-01-03T04%3A22%3A55Z&se=2019-04-04T04%3A22%3A00Z&sig=%2F1i7SsE%2FAqLKuGILJUrMZMfRwPE5DhufwFxow70rQgQ%3D', true);
+    xobj.open('GET', 'https://smashappstorage.file.core.windows.net/smashappfiles/images/moves/character_general_data.json' + '?st=2019-01-04T04%3A38%3A14Z&se=2020-01-05T04%3A38%3A00Z&sp=rl&sv=2018-03-28&sr=s&sig=HEgPEvrg%2F34yozJPQPfkKQANMpifiQnsKNddAFGfi2s%3D', true);
 
     xobj.onreadystatechange = function () {
           if (xobj.readyState == 4 && xobj.status == "200") {
